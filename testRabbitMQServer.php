5092cb0845a05bed44d6881bb2dd43506adbc34b
engineerOfLies <donald.j.kehoe@gmail.com>
2015-09-25 20:58:08 -0400

    more robust, still not working

---
+++ testRabbitMQServer.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function doLogin($username,$password)
+{
+    // lookup username in databas
+    // check password
+    return true;
+    //return false if not valid
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId'])
+  }
+  return "received request";
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+$server->process_requests('requestProcessor');
+?>
+
