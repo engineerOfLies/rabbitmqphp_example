30a815b573dcd526111764e84b2f97dae0dc4506
DJ Kehoe <kehoed@njit.edu>
2018-01-29 17:26:18 -0500

    building a basic php mysql sample

---
+++ mysqlconnect.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+
+$mydb = new mysqli('127.0.0.1','root','12345','testdb');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+	exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "select * from students;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+
+
+?>
