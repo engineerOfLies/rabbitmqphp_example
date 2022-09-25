f349bcb18778ee2b9b180cb4b3278c4d1b4cb8d3
ji64 <ji64@njit.edu>
2022-09-25 02:18:59 +0000

    reorganizing files

---
+++ RabbitMQ/testRabbitMQClient.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+$request = array();
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
