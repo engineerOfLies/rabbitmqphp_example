de55894c49f006a1767b2e5d58fb2eff69e4dc9d
priyapatel7 <pp766@njit.edu>
2022-10-05 12:38:48 -0400

    commit 1

---
+++ mysqlconnect.php
@@ -0,0 +1,39 @@
+#!/usr/bin/php
+<?php
+
+$mydb = new mysqli('123.0.0.1', 'testUser', '12345', 'testdb');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: " . $mydb->error . PHP_EOL;
+	exit(0);
+
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "select * from testtable;";
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__. ':' .__LINE__. "error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+$numrows = mysqli_num_rows($response);
+
+echo "we got $numrows rows from the query".PHP-EOL;
+
+while ($row = $response ->fetch_row())
+{
+	print_r($row);
+}
+
+echo "test complete".PHP_EOL;
+
+?>
+
+
+
+
+
