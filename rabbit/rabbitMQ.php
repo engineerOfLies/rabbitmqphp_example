f840c8a49e2745a2676ee7acd3c1e4b32eed682b
cjdomacena <cd395@njit.edu>
2023-02-07 18:06:49 -0500

    added checks for null values from sql query result

--- rabbit/rabbitMQ.php
+++ rabbit/rabbitMQ.php
@@ -19,48 +19,60 @@ function dbConnect($request)
         case "login":
             $stmt = mysqli_prepare($db, "SELECT * FROM users WHERE username = '{$data['username']}'");
             $stmt->execute();
-            if($stmt->errno !== 0) {
+            if ($stmt->errno !== 0) {
                 die("Failed to execute query" . $stmt->error);
             }
 
             $result = $stmt->get_result()->fetch_assoc();
-            if($result["user_pass"] == $data["user_pass"] && $result["username"] == $data['username']) {
-                return array("code"=> 0, "message"=> $result);
-            } 
-            else if ($result["user_pass"] != $data["user_pass"] && $result["username"] == $data['username']){
-                return array("code"=> 1, "message"=> $result);
-            }      
-            else if ($result["user_pass"] != $data["user_pass"] && $result["username"] != $data['username']) {
-                return array("code" => 2, "message"=> $result);
+
+            // Checks if user is found
+            if (array($result)[0] !== NULL) {
+                if ($result["user_pass"] == $data["user_pass"] && $result["username"] == $data['username']) {
+                    // echo and var_dump on terminal
+                    echo "USER FOUND!".PHP_EOL;
+                    var_dump($request);
+                    return array("code" => 0, "message" => $result);
+                } else {
+                    // If first (if statement) fails, automatically fail since we don't want to show which one was wrong.
+                    echo "INVALID Credentials!".PHP_EOL;
+                    var_dump($request);
+                    return array("code" => 1, "message" => "Invalid Credentials");
+                }
+            } else {
+                echo "USER NOT FOUND".PHP_EOL;
+                var_dump($request);
+                return array("code" => 2, "message" => "Account not found.");
+
             }
+
         case "create":
             $stmt = mysqli_prepare($db, "INSERT INTO users(username, user_pass)VALUES
             ('{$data['username']}', '{$data['user_pass']}')");
             $stmt->execute();
-            return array("code"=> 0, "message"=> "WORKSIUJASHTUAISGTYQUIAEWTGQ");
-            // $stmt = mysqli_prepare($db, "SELECT * FROM users");
-            // $stmt->execute();
-            // if($stmt->errno !== 0) {
-            //     die("Failed to execute query" . $stmt->error);
-            // }
-            // $query = "SELECT * FROM users WHERE username = 'test'";
-            // $find = mysqli_query($db, $query);
+            return array("code" => 0, "message" => "WORKSIUJASHTUAISGTYQUIAEWTGQ");
+        // $stmt = mysqli_prepare($db, "SELECT * FROM users");
+        // $stmt->execute();
+        // if($stmt->errno !== 0) {
+        //     die("Failed to execute query" . $stmt->error);
+        // }
+        // $query = "SELECT * FROM users WHERE username = 'test'";
+        // $find = mysqli_query($db, $query);
 
-            // if(mysqli_num_rows($find) != 0) {
-            //     return array("code" => 0, "message" => $find);
-            // }
-            // else {
-            //     $insert="INSERT INTO users(username, user_pass)
-            //     VALUES ('it','it')";
-            // }
+        // if(mysqli_num_rows($find) != 0) {
+        //     return array("code" => 0, "message" => $find);
+        // }
+        // else {
+        //     $insert="INSERT INTO users(username, user_pass)
+        //     VALUES ('it','it')";
+        // }
 
-            
-        }
-        
+
+    }
+
 }
 
-$server = new rabbitMQServer(__DIR__."/../lib/config/rabbitMQ.ini", "rabbit");
-echo "Server started...";
+$server = new rabbitMQServer(__DIR__ . "/../lib/config/rabbitMQ.ini", "rabbit");
+echo "Server started..." . PHP_EOL;
 $server->process_requests("dbConnect");
 
 ?>
