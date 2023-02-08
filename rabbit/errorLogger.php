cb0cb55514fb72d0c91914843aaa84249d289487
js2499 <js2499@njit.edu>
2023-02-08 18:19:08 -0500

    errorfunction

--- rabbit/errorLogger.php
+++ rabbit/errorLogger.php
@@ -4,16 +4,13 @@ require_once(__DIR__ . "/../lib/config/path.inc");
 require_once(__DIR__ . "/../lib/rabbitMQLib.inc");
 require_once(__DIR__ . "/../lib/config/rabbitMQ.ini");
 
-function errorHandler($request) {
-    $error_message = $request['error_message'];
-
-    $error_log_file = __DIR__. "/error.log";
-    $timestamp = date("Y-n-d H:i:s");
-    $log_entry = "$timestamp: $error_message\n";
-    file_put_contents($error_log_file. $log_entry, FILE_APPEND);
-
-    return true;
-}
+//function errorHandler($request) {
+    $file = fopen('errorLog.txt', 'a+');
+    fwrite($file, "hell"."<br>");
+    fclose($file);
+ 
+    echo 'works'.PHP_EOL;
+//}
 
 $server = new rabbitMQServer(__DIR__."/../lib/config/rabbitMQ.ini", "rabbit");
 echo "Server started...";
