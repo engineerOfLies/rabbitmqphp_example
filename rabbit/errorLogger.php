803cc12b805c0b46fdc3aee6160d67157eeb4966
jk259 <jk259@IT490.myguest.virtualbox.org>
2023-02-08 18:41:43 -0500

    added errorlogger.ini and changed path in php file

--- rabbit/errorLogger.php
+++ rabbit/errorLogger.php
@@ -2,7 +2,7 @@
 <?php
 require_once(__DIR__ . "/../lib/config/path.inc");
 require_once(__DIR__ . "/../lib/rabbitMQLib.inc");
-require_once(__DIR__ . "/../lib/config/rabbitMQ.ini");
+require_once(__DIR__ . "/../lib/config/errorLogger.ini");
 
 //function errorHandler($request) {
     $file = fopen('errorLog.txt', 'a+');
