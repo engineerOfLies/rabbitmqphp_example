937332b7634daf593d4efc9732038331435cb17c
jk259 <jk259@IT490.myguest.virtualbox.org>
2023-02-08 17:21:30 -0500

    updated errorLogger.php

---
+++ rabbit/errorLogger.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?php
+require_once(__DIR__ . "/../lib/config/path.inc");
+require_once(__DIR__ . "/../lib/rabbitMQLib.inc");
+require_once(__DIR__ . "/../lib/config/rabbitMQ.ini");
+
+function errorHandler($request) {
+    $error_message = $request['error_message'];
+
+    $error_log_file = _DIR_. "/error.log";
+    $timestamp = date("Y-n-d H:i:s");
+    $log_entry = 'Stimestamp; $error_message\n';
+    file_put_contents($error_log_file. $log_entry, FILE_APPEND);
+
+    return true
+}
+
+$server = new rabbitMQServer(__DIR__."/../lib/config/rabbitMQ.ini", "rabbit");
+echo "Server started...";
+$server->process_requests("errorHandler");
+?>
