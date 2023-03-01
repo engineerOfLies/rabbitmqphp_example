35ef4df1fad464ade902cb0305c4fe93ed3f2d5c
jk259 <jk259@IT490.myguest.virtualbox.org>
2023-03-01 14:59:55 -0500

    adding error logger to dmzRabbit.php

--- rabbit/dmzRabbit.php
+++ rabbit/dmzRabbit.php
@@ -35,6 +35,9 @@ require_once(__DIR__ . "/../lib/config/rabbitMQ.ini");
                 echo $imdbArr['Plot'];
                 // incase i forget
                 // grab id -> fetch imdb data -> send shit back -> creates new page -> lets user view
+                if($stmt->errno !== 0) {
+                    send(array("data" => array("error" => $stmt->error)), "error");
+                }
                 return array("code" => 0, "message" => $imdbArr);
     }
 
