a44d648d0dc5c557161124643a5b2983d6e7df61
js2499 <js2499@njit.edu>
2023-03-06 05:19:06 -0500

    forum branch+very basic html

---
+++ frontend/forum.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+  
+ 
+  <body>
+    <header>
+      <h1>Forum</h1>
+    </header>
+   
+    <main>
+      <section>
+        <h2>Thread Title</h2>
+        <article>
+          <header>
+            <h3>Post Title</h3>
+            <p>Posted by User on Date</p>
+          
+        </header>
+         
+        <p>Post Content</p>
+        </article>
+       
+        <article>
+          
+        <header>
+            <h3>Reply Title</h3>
+            <p>Posted by User on Date</p>
+          </header>
+          
+          <p>Reply Content</p>
+       
+        </article>
+      </section>
+    </main>
+   
+    <footer>
+      <p> Forum</p>
+    </footer>
+ 
+</body>
+</html>
