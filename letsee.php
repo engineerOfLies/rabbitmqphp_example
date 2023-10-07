1a3297c2d38c12e81fe7babd9c3e458f850a3445
Hareem Awan <114123490+hareemua4@users.noreply.github.com>
2023-10-07 16:46:53 -0400

    update image url

--- letsee.php
+++ letsee.php
@@ -1,6 +1,5 @@
 <?php
-    if(isset($_POST['buttons'])){
-        //your code
-       header('Location:successfulloginpage.html?status=success');//redirect to your html with status
-    }
+
+echo "Hello";
 ?>
+
