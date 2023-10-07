dba54f9b6d3003d8f4cdd4b6df6cb040edad3438
Hareem Awan <114123490+hareemua4@users.noreply.github.com>
2023-10-07 19:11:36 -0400

    update

--- letsee.php
+++ letsee.php
@@ -1,5 +1,21 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
-echo "Hello";
+$client = new rabbitMQClient("testRabbitMQ.ini","databaseServer");
+
+$request = array();
+$request['type'] = "login";
+$request['username'] = $_POST["username"];
+$request['password'] = $_POST["password"];
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
 ?>
-
