e0d107f13846200c006d38e1d644067957be3760
Michael Capetillo <mjc223@njit.edu>
2023-10-11 17:58:02 -0400

    Beginning refactoring. Breaking changes

--- src/php/loginbase.php
+++ src/php/loginbase.php
@@ -1,7 +1,5 @@
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
+require_once('../include/loginbase.php');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","databaseServer");
 
