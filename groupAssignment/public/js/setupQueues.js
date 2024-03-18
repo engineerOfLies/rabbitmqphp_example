33f2440c065ed9f8277aa8a09a26cf9c34f5ee14
Yardley Francois <144744560+YardleyFra@users.noreply.github.com>
2024-03-18 19:26:29 -0400

    group_Assignment

--- groupAssignment/public/js/setupQueues.js
+++
@@ -1,9 +0,0 @@
-const { setupQueue } = require('./rabbitmqService');
-
-setupQueue().then(() => {
-  console.log('Queue setup completed');
-  process.exit(0);
-}).catch(error => {
-  console.error('Failed to setup the queue:', error);
-  process.exit(1);
-});
