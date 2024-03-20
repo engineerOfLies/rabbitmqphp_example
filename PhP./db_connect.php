6f4a96585ad504d9d16f6352f0981976c95cdda0
Yardley Francois <144744560+YardleyFra@users.noreply.github.com>
2024-03-19 21:13:41 -0400

    update

--- PhP./db_connect.php
+++ PhP./db_connect.php
@@ -1,14 +1,29 @@
 <?php
-$db_name = "test_db";
-$sname = "localhost";
-$uname = "root";
-$password = "";
+$db_host = "remote_mysql_host";
+$db_name = "remote_mysql_db";
+$db_username = "remote_mysql_username";
+$db_password = "remote_mysql_password";
 
-$conn = mysqli_connect($sname, $uname, $password, $db_name);
+$mq_host = "rabbitmq_host";
+$mq_port = "rabbitmq_port";
+$mq_username = "rabbitmq_username";
+$mq_password = "rabbitmq_password";
+
+// Connect to MySQL
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("MySQL connection failed: " . mysqli_connect_error());
 } else {
-    echo "Connected successfully";
+    echo "MySQL connected successfully\n";
+}
+
+// Connect to RabbitMQ
+$mq_conn = new AMQPStreamConnection($mq_host, $mq_port, $mq_username, $mq_password);
+
+if (!$mq_conn) {
+    die("RabbitMQ connection failed\n");
+} else {
+    echo "RabbitMQ connected successfully\n";
 }
 ?>
